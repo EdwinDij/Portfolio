9a5cf73e09203c2e9cf6a1c0d6c972f2ad9fdb5f
EdwinDij <edwin.d899@gmail.com>
2022-05-29 14:02:15 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Main from './Components/Présentation/Main';
+
+
+function App() {
+  return (
+    <Main />
+
+  )
+}
+
+export default App;
---
+++ src/Components/Présentation/Main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Main() {
+    return(
+        <div>Bonjour</div>
+    )
+}
+export default Main
